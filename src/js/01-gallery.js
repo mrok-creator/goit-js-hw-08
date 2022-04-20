7583c46dd1daafd6590705c89718520863325818
Kasianenko Oleksandr <sanekkas@gmail.com>
2022-04-20 16:56:32 +0300

    add task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+
+function createGalleryMarkup(arr) {
+  return arr
+    .map(
+      ({ preview, original, description }) => `
+  <a class="gallery__item" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+     </a>
+  `,
+    )
+    .join('');
+}
+galleryRef.insertAdjacentHTML('beforeend', createGalleryMarkup(galleryItems));
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
