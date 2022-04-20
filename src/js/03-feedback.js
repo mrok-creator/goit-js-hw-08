8b63895d6bd5a8fb283e7ff87c38f6cbb3978b9f
Kasianenko Oleksandr <sanekkas@gmail.com>
2022-04-20 21:11:30 +0300

    add work with locale storage task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+import throttle from 'lodash.throttle';
+const ref = {
+  parent: document.querySelector('.feedback-form'),
+  email: document.querySelector('label>input'),
+  message: document.querySelector('label> textarea'),
+};
 
+getToLocalSTorage();
+ref.parent.addEventListener('input', throttle(saveFormData), 500);
+
+function saveFormData(event) {
+  const formData = objDataCreate(ref.email.value, ref.message.value);
+  addToLocalStorage(formData);
+}
+
+function objDataCreate(email, text) {
+  return {
+    email,
+    text,
+  };
+}
+
+function addToLocalStorage(obj, key = 'feedback - form - state') {
+  localStorage.setItem(key, JSON.stringify(obj));
+}
+
+function getToLocalSTorage(key = 'feedback - form - state') {
+  const stringData = localStorage.getItem(key);
+  if (stringData) {
+    const data = JSON.parse(stringData);
+
+    ref.email.value = data.email;
+    ref.message.value = data.text;
+  }
+  return;
+}
